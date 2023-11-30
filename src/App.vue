4eae03a95fbf58d674c088d4a1de238142eeed90
unknown <muhamadrizkihasan@smkwikrama.sch.id>
2023-11-30 20:58:16 +0700

    Menambahkan folder card-vueJS

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- Jumlah card sama dengan jumlah data dari card Contents -->
+    <Card v-for="(item, index) in cardContents" :key="index" :judul="item.title" :text="item.text" :image="item.image">
+        <!-- Kodingan yang ada di tengah component bakal masuk ke slot -->
+        <small>created by: saya</small>
+    </Card>
+</template>
+
+<script>
+// Panggil file
+import Card from '@/components/Card.vue';
+export default {
+    // Component apa saja yang akan ditampilkan 
+    components: {
+        // Definisi component agar bisa digunakan di template
+        Card
+    },
+    data() {
+        return {
+            cardContents: [
+                {
+                    title: 'Cara Bernafas : ',
+                    text: `<ul>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, recusandae?</li>
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                        </ul>`,
+                    image: 'ncs.jpg',
+                }, {
+                    title: 'Cara Makan : ',
+                    text: `<ul>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, recusandae?</li>
+                        <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                        </ul>`,
+                    image: 'ncs.jpg',
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style></style>
---
+++ src/components/Card.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="wrap d-flex mt-3">
+        <div class="card bg-secondary d-flex flex-row">
+            <div class="img-wrap">
+                <img :src="'/src/assets/img/' + image" alt="No Copyright Sound">
+            </div>
+            <div class="text-wrap">
+                <h1>{{ judul }}</h1>
+                <slot></slot>
+                <div v-html="text"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        // Mengambil data dari luar / file lain
+        // Nama props == nama bind attribute di componentnya tempat data dikirim
+        props: ['judul', 'text', 'image']
+    }
+</script>
+
+<style scoped>
+    .wrap {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+    
+    .card {
+        display: flex;
+        width: 600px;
+        height: auto;
+        padding: 10px;
+        border-radius: 10px;
+        border: 3px black solid;
+    }
+    .img-wrap {
+        width: 35%;
+        height: 100%;
+        position: relative;
+    }
+    .img-wrap img {
+        width: 100%;
+        height: 100%;
+        /* position: absolute; */
+    }
+
+    .text-wrap {
+        width: 65%;
+        height: 100%;
+        padding: 10px;
+    }
+</style>
